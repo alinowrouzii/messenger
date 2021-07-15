2fa79efc994c358ad04a086f6ac7e9d0370b21d4
AliNowrouzi <alinowrouzii@gmail.com>
2021-07-16 04:23:39 +0430

    Add passport.js configuration to index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
-// import bodyParser  from 'body-parser';
 import mongoose from 'mongoose';
 import passport from 'passport';
 import session from 'express-session';
@@ -11,8 +10,6 @@ import userRoute from './route/user.js';
 import messageRoute from './route/message.js';
 import chatRoute from './route/chat.js';
 
-import User from './models/User.js';
-
 const app = express();
 
 app.use(express.json({ limit: '20mb', extended: true }))
@@ -20,7 +17,6 @@ app.use(express.urlencoded({ limit: '20mb', extended: true }))
 app.use(cors());
 
 app.use(session({
-  //TODO: change this secret .env
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false
@@ -29,22 +25,19 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-// import LocalStrategy from 'passport-local';
-// passport.use(new LocalStrategy.Strategy(User.authenticate()));
+//configuration of passport.js
+import * as pass from './config/passport.js'
 
-passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
 
-const MONGIOSE_URL = process.env.MONGOOSE_URL;
+const MONGIOSE_URL = process.env.MONGOOSE_URL_2;
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(MONGIOSE_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
-.then(() => app.listen(PORT, () => console.log(`Server is running on Port: ${PORT}`)))
-.catch((error) => console.log(`${error} mongoDB didn't connect!`));
+  .then(() => app.listen(PORT, () => console.log(`Server is running on Port: ${PORT}`)))
+  .catch((error) => console.log(`${error} mongoDB didn't connect!`));
 
 mongoose.set('useCreateIndex', true);
 mongoose.set('useFindAndModify', false);
