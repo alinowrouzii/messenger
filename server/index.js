3ce5908449464511913e315e053072c999a6a839
AliNowrouzi <alinowrouzii@gmail.com>
2021-07-16 20:26:55 +0430

    import configuration of mongoose and passport

--- server/index.js
+++ server/index.js
@@ -2,9 +2,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
-import mongoose from 'mongoose';
 import passport from 'passport';
 import session from 'express-session';
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import userRoute from './route/user.js';
 import messageRoute from './route/message.js';
@@ -14,7 +14,10 @@ const app = express();
 
 app.use(express.json({ limit: '20mb', extended: true }))
 app.use(express.urlencoded({ limit: '20mb', extended: true }))
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true,
+}));
 
 app.use(session({
   secret: process.env.SESSION_SECRET,
@@ -22,26 +25,21 @@ app.use(session({
   saveUninitialized: false
 }));
 
+app.use(cookieParser(process.env.SESSION_SECRET))
 app.use(passport.initialize());
 app.use(passport.session());
 
 //configuration of passport.js
-import * as pass from './config/passport.js'
+import { passportConfig } from './config/passport.js';
+passportConfig(passport);
 
-
-const MONGIOSE_URL = process.env.MONGOOSE_URL_2;
-const PORT = process.env.PORT || 5000;
-
-mongoose.connect(MONGIOSE_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-  .then(() => app.listen(PORT, () => console.log(`Server is running on Port: ${PORT}`)))
-  .catch((error) => console.log(`${error} mongoDB didn't connect!`));
-
-mongoose.set('useCreateIndex', true);
-mongoose.set('useFindAndModify', false);
+//mongoDB connection and config
+import { mongooseConfig } from './config/mongooseConfig.js';
+mongooseConfig();
 
 app.use('/user', userRoute);
 app.use('/message', messageRoute);
 app.use('/chat', chatRoute);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running on Port: ${PORT}`));
