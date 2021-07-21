b283bd1a12ab8f3acc2a0b9487d0dce63780dd32
AliNowrouzi <alinowrouzii@gmail.com>
2021-07-22 03:57:04 +0430

    Add profile section to git

--- client/src/components/Utils/Profile/Profile.js
+++ client/src/components/Utils/Profile/Profile.js
@@ -4,7 +4,7 @@ import { Card, Image } from 'react-bootstrap';
 import img from './../../../Images/no-profile.jpg'
 const Profile = () => {
     return (
-        <Card style={{ width: '18rem', padding: "20px", backgroundColor: "lightcyan", marginTop: "20px" }}>
+        <Card style={{ width: 'auto', padding: "20px", backgroundColor: "lightcyan", marginTop: "20px" }}>
             <Card.Img as={Image} variant="top" src={img} roundedCircle />
             <Card.Body>
                 <Card.Title style={{ textAlign: "center" }}>
