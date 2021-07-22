7107b1739d2f01e595480fdb313c75c5bf422228
AliNowrouzi <alinowrouzii@gmail.com>
2021-07-22 23:13:38 +0430

    Add useSelector to conditionally render chatPage or loginPage

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,15 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import ChatPage from './../ChatPage/ChatPage';
 import Login from './../Login/Login';
+import { useSelector } from 'react-redux';
 const Home = () => {
 
     //isLoggedin should be global and we should use redux and use isLoggedIn globally
-    const [isLoggedin, setLogin] = useState(true);
-
+    const isLoggedIn = useSelector(state => state.userData.isLoggedIn);
+    useEffect(()=>{
+        console.log(isLoggedIn)
+    });
     return (
         <>
-        home
+            {isLoggedIn ? <ChatPage/> : <Login/>}
         </>
     );
 };
