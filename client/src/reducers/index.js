e95100b866104818e0f03cf4031096a6f2dd0955
AliNowrouzi <alinowrouzii@gmail.com>
2021-07-23 19:40:56 +0430

    Update combineReducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
+import auth from "./authReducer"
 import userData from "./userReducer";
+import chatsData from "./chatReducer";
 
 export default combineReducers({
-    userData,
-  });
+  userData, auth, chatsData,
+});
