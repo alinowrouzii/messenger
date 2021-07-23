ebc52d8e23585bb57a6d859fa70868e738b2ca7b
AliNowrouzi <alinowrouzii@gmail.com>
2021-07-23 19:40:36 +0430

    Remove auth actions

--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -1,51 +1,39 @@
 import {
-    REGISTER_SUCCESS,
-    REGISTER_FAIL,
-    LOGIN_SUCCESS,
-    LOGIN_FAIL,
-    LOGOUT,
+    FETCH_ME_SUCCESS,
+    FETCH_ME_FAIL,
+    LOGOUT
 } from "../actions/types";
 
-const initialState = { isLoggedIn: false, loginRegMessage: "", user: null };
+const initialState = { ownUser: null, ownUserMessage:"" };
 
 export default (state = initialState, action) => {
     const { type, payload } = action;
     console.log(type, payload);
     
     switch (type) {
-        case REGISTER_SUCCESS:
+        case FETCH_ME_SUCCESS:
             return {
                 ...state,
-                isLoggedIn: false,
-                loginRegMessage: payload.message
+                ownUser: payload.user,
+                ownUserMessage: payload.message
             };
-        case REGISTER_FAIL:
+        case FETCH_ME_FAIL:
             return {
                 ...state,
-                isLoggedIn: false,
-                loginRegMessage: payload.message
+                ownUserMessage: payload.message
             };
-        case LOGIN_SUCCESS:
-            return {
-                ...state,
-                isLoggedIn: true,
-                user: payload.user,
-                loginRegMessage: payload.message
-            };
-        case LOGIN_FAIL:
-            return {
-                ...state,
-                isLoggedIn: false,
-                user: null,
-                loginRegMessage: payload.message
-            };
+        // case FETCH_CONTACT_USERS_SUCCESS:
+        //     return {
+        //         ...state,
+        //         ownUserMessage: payload.message
+        //     };
+        // case FETCH_CONTACT_USERS_FAIL:
+        //     return {
+        //         ...state,
+        //         ownUserMessage: payload.message
+        //     };
         case LOGOUT:
-            return {
-                ...state,
-                isLoggedIn: false,
-                user: null,
-                loginRegMessage: ""
-            };
+            return initialState;
         default:
             return state;
     }
