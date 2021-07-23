df27534aa21fc6c583d693d35272ed88fd7bf0b1
AliNowrouzi <alinowrouzii@gmail.com>
2021-07-23 06:04:52 +0430

    remove SET_LOGIN_REG_MESSAGE action and add it into another actions

--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -4,49 +4,47 @@ import {
     LOGIN_SUCCESS,
     LOGIN_FAIL,
     LOGOUT,
-    SET_LOGIN_REG_MESSAGE
 } from "../actions/types";
 
 const initialState = { isLoggedIn: false, loginRegMessage: "", user: null };
 
 export default (state = initialState, action) => {
     const { type, payload } = action;
-
     console.log(type, payload);
-
+    
     switch (type) {
         case REGISTER_SUCCESS:
             return {
                 ...state,
                 isLoggedIn: false,
+                loginRegMessage: payload.message
             };
         case REGISTER_FAIL:
             return {
                 ...state,
                 isLoggedIn: false,
+                loginRegMessage: payload.message
             };
         case LOGIN_SUCCESS:
             return {
                 ...state,
                 isLoggedIn: true,
                 user: payload.user,
+                loginRegMessage: payload.message
             };
         case LOGIN_FAIL:
             return {
                 ...state,
                 isLoggedIn: false,
                 user: null,
+                loginRegMessage: payload.message
             };
         case LOGOUT:
             return {
                 ...state,
                 isLoggedIn: false,
                 user: null,
-            };
-        case SET_LOGIN_REG_MESSAGE:
-            return {
-                ...state,
-                loginRegMessage: payload.message,
+                loginRegMessage: ""
             };
         default:
             return state;
