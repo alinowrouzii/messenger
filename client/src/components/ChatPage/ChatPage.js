08abf6cdc3f0f26ab1d395101f61a9fbd59542c8
AliNowrouzi <alinowrouzii@gmail.com>
2021-07-23 19:44:08 +0430

    Add chats and some states

--- client/src/components/ChatPage/ChatPage.js
+++ client/src/components/ChatPage/ChatPage.js
@@ -7,7 +7,14 @@ import 'emoji-mart/css/emoji-mart.css'
 import { Picker } from 'emoji-mart'
 import Profile from '../Utils/Profile/Profile';
 import './styles.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout } from '../../actions/auth';
+import { getMe, getUserData } from '../../actions/user';
+import { getChats } from '../../actions/chat'
+
 const ChatPage = () => {
+
+    const dispatch = useDispatch();
 
     const [typedText, setTypedText] = useState("");
 
@@ -16,15 +23,37 @@ const ChatPage = () => {
         scrollRef.current?.scrollIntoView({ behavior: "smooth" });
     }, [])
 
+    const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+
+    const ownUser = useSelector(state => state.userData.ownUser);
+    const chats = useSelector(state => state.chatsData.chats);
+
+    useEffect(() => {
+        if (isLoggedIn) {
+            dispatch(getChats())
+                .then(() => {
+                    console.log('chats fetched successfuly');
+                }).catch((err) => {
+                    console.log('error in fetching chats');
+                });;
+        }
+    }, [isLoggedIn]);
+
+    const handleLogout = (e) => {
+        dispatch(logout());
+    }
+
+
+
     return (
         <>
             <Container className="mainContainer chatPageStyles" fluid>
                 <Row className="rowOne mb-3">
-                    <Col lg={4} style={{ marginTop: "15px", marginBottom:"10px" }}>
+                    <Col lg={4} style={{ marginTop: "15px", marginBottom: "10px" }}>
                         <MyProfileInfo />
                     </Col>
                     <Col lg={8}>
-                        col12
+                        <Button onClick={handleLogout}>logout</Button>
                     </Col>
                 </Row>
                 <Row className="rowTwo">
@@ -38,7 +67,7 @@ const ChatPage = () => {
                         </Row>
                         <Row>
                             <InputGroup className="mb-3">
-                                <FormControl aria-label="Enter the username!" placeHolder="Enter userName" />
+                                <FormControl aria-label="Enter the username!" placeholder="Enter userName" />
                                 <Button variant="outline-secondary">Search!</Button>
                             </InputGroup>
                         </Row>
@@ -107,13 +136,13 @@ const ChatPage = () => {
                             </div>
                             <InputGroup className="sendSection">
                                 {/* <Picker style={{ position: 'absolute', bottom: '10px', left: '10px' }} set='apple' onSelect={(e) => setTypedText(prevtext => (prevtext + e.native))} title='Pick your emoji…' emoji='point_up' emojiTooltip={true} /> */}
-                                <FormControl as="textarea" className="textarea" onChange={(e) => setTypedText(e.target.value)} value={typedText} placeHolder="type something" />
+                                <FormControl as="textarea" className="textarea" onChange={(e) => setTypedText(e.target.value)} value={typedText} placeholder="type something" />
                                 <Button variant="primary">Send!</Button>
                             </InputGroup>
                         </div>
                     </Col>
                     <Col lg={2}>
-                        <div class="profileSection">
+                        <div className="profileSection">
                             <Profile></Profile>
                         </div>
                     </Col>
