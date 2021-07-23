46947c6401beb5893a8eac6079e276daafe56356
AliNowrouzi <alinowrouzii@gmail.com>
2021-07-23 05:14:07 +0430

    Wrap components into PersistGate

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { useSelector } from 'react-redux';
 import { Provider } from "react-redux";
-import store from './store.js'
+import storePersistor from './store.js'
 import React, { useEffect, useState } from 'react';
 import About from './components/About/About';
 import Home from './components/Home/Home';
@@ -10,21 +10,27 @@ import Login from './components/Login/Login';
 import Navbar from './components/Navbar/Navbar'
 import ChatPage from './components/ChatPage/ChatPage';
 import './styles.css'
+import { PersistGate } from "redux-persist/integration/react";
 function App() {
+
+  const { store, persistor } = storePersistor();
 
   return (
     <Provider store={store}>
-      <div className="app-cont">
-        <Router>
-          {/* <Navbar/> */}
-          <Switch>
-            <Route path="/about" component={About} />
-            <Route exact path="/" component={Home} />
-            <Route path="/login" component={Login} />
+      <PersistGate loading={null} persistor={persistor}>
 
-          </Switch>
-        </Router>
-      </div>
+        <div className="app-cont">
+          <Router>
+            {/* <Navbar/> */}
+            <Switch>
+              <Route path="/about" component={About} />
+              <Route exact path="/" component={Home} />
+              <Route path="/login" component={Login} />
+
+            </Switch>
+          </Router>
+        </div>
+      </PersistGate>
     </Provider>
   );
 }
