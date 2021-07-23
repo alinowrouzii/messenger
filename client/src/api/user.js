5c29eaea3636ade3e76a091d8576eb53be0874c4
AliNowrouzi <alinowrouzii@gmail.com>
2021-07-23 19:39:29 +0430

    Remove auth requests

--- client/src/api/user.js
+++ client/src/api/user.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 import { URL } from '../constants';
 
-export const login = (userPass) => axios.post(`${URL}/user/login`, userPass, { withCredentials: true });
-export const logout = () => axios.get(`${URL}/user/logout`, { withCredentials: true });
-export const signup = (newUser) => axios.post(`${URL}/user/signup`, newUser, { withCredentials: true });
 export const fetchMe = () => axios.get(`${URL}/user`, { withCredentials: true });
 export const fetchUserData = (userId) => axios.get(`${URL}/user/getUser/${userId}`, { withCredentials: true });
+export const fetchContactUsers = () => axios.get(`${URL}/user/getContactUsers`, { withCredentials: true });
