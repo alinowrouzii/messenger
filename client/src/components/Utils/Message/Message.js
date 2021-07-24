db795b5053559b2394a13feb823be77745fd14b6
AliNowrouzi <alinowrouzii@gmail.com>
2021-07-25 01:41:40 +0430

    Add props as argument and use format to format the time

--- client/src/components/Utils/Message/Message.js
+++ client/src/components/Utils/Message/Message.js
@@ -1,51 +1,54 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
 import img from './../../../Images/no-profile.jpg';
+
+import { format } from "timeago.js";
 import './styles.css';
 
 
-const Message = (props) => {
-
+const Message = ({ me, msg }) => {
 
     return (
         <>
-            {props.me ?
+            {me ?
                 <div>
-                    <div class="d-flex align-items-center mb-3">
-                        <div class="flex-grow-1 ms-1">
-                            <Card 
-                            className="mb-2 shadow-lg p-3" 
-                            style={{ maxWidth: '18rem', float: 'right', marginRight: '20px', backgroundColor:"cyan" }}>
+                    <div className="d-flex align-items-center mb-3">
+                        <div className="flex-grow-1 ms-1">
+                            <Card
+                                className="mb-2 shadow-lg p-3"
+                                style={{ maxWidth: '18rem', float: 'right', marginRight: '20px', backgroundColor: "cyan" }}>
                                 <Card.Body>
                                     <Card.Text>
-                                        {props.text}
+                                        {msg.text}
                                     </Card.Text>
                                 </Card.Body>
                                 <Card.Body>
-                                    <Card.Text> 3 min ago</Card.Text>
+                                    <Card.Text> {format(msg.createdAt)}</Card.Text>
                                 </Card.Body>
                             </Card>
                         </div>
-                        <div class="flex-shrink-0">
-                            <img src={img} class="rounded-circle prof-img" alt="img" />
+                        <div className="flex-shrink-0">
+                            <img src={img} className="rounded-circle prof-img" alt="img" />
                         </div>
                     </div>
 
                 </div>
                 :
-                <div class="d-flex align-items-center mb-3">
-                    <div class="flex-shrink-0">
-                        <img src={img} class="rounded-circle prof-img" alt="img" />
+                <div className="d-flex align-items-center mb-3">
+                    <div className="flex-shrink-0">
+                        <img src={img} className="rounded-circle prof-img" alt="img" />
                     </div>
-                    <div class="flex-grow-1 ms-1">
-                        <Card className="mb-2 shadow-lg p-3 bg-white" style={{ float: "left", maxWidth: '18rem' }}>
+                    <div className="flex-grow-1 ms-1">
+                        <Card
+                            className="mb-2 shadow-lg p-3 bg-green"
+                            style={{ float: "left", maxWidth: '18rem', backgroundColor:"#f590f5" }}>
                             <Card.Body>
                                 <Card.Text>
-                                    {props.text}
+                                    {msg.text}
                                 </Card.Text>
                             </Card.Body>
                             <Card.Body>
-                                <Card.Text> 2 min ago</Card.Text>
+                                <Card.Text>{format(msg.createdAt)}</Card.Text>
                             </Card.Body>
                         </Card>
                     </div>
