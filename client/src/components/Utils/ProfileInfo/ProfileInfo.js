41833840b9c1f022b4fd79296c2038e53bf6328b
AliNowrouzi <alinowrouzii@gmail.com>
2021-07-25 01:45:35 +0430

    Add some style

--- client/src/components/Utils/ProfileInfo/ProfileInfo.js
+++ client/src/components/Utils/ProfileInfo/ProfileInfo.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import img from "./../../../Images/no-profile.jpg";
 import './styles.css'
-const ProfileInfo = () => {
-    return (
+const ProfileInfo = (props) => {
 
-        <div class="d-flex align-items-center main-cont rounded">
-            <div class="flex-shrink-0">
-                <img src={img} class="rounded-circle prof-img" alt="img" />
+    const ownUser = useSelector(state => state.userData.ownUser);
+    const ownUserIsReady = useSelector(state => state.userData.ownUserIsReady);
+
+    const chat = props.chat;
+
+    const [userInfo, setUserInfo] = useState(null);
+
+    useEffect(() => {
+        setUserInfo(
+            (ownUser?._id === chat.users[0]._id) ? chat.users[1] : chat.users[0]
+        )
+    }, [ownUserIsReady]);
+    
+    return (
+        <div className="d-flex align-items-center main-cont rounded bg-info shadow-lg">
+            <div className="flex-shrink-0 img-cont">
+                <img src={img} className="rounded-circle prof-img" alt="img" />
             </div>
-            <div class="flex-grow-1 ms-3">
-                AliNowrouzi<br />
+            <div className="flex-grow-1 ms-3">
+                {ownUserIsReady ? userInfo?.name : "loading"}<br />
                 <p>
                     online
                 </p>
