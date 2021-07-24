1ebfa1bc60dbfe55646b726d6f270a6babdedb09
AliNowrouzi <alinowrouzii@gmail.com>
2021-07-25 01:44:26 +0430

    Add functionality to userProfile and use fetched data

--- client/src/components/Utils/Profile/Profile.js
+++ client/src/components/Utils/Profile/Profile.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import './styles.css'
 import { Card, Image } from 'react-bootstrap';
+import { format } from 'timeago.js';
 import img from './../../../Images/no-profile.jpg'
-const Profile = () => {
+const Profile = (props) => {
     return (
-        <Card style={{ width: 'auto', padding: "20px", backgroundColor: "lightcyan", marginTop: "20px" }}>
+        <Card
+            style={{ width: 'auto', padding: "20px", backgroundColor: "cyan", marginTop: "20px" }}
+            className="shadow-lg"
+        >
             <Card.Img as={Image} variant="top" src={img} roundedCircle />
             <Card.Body>
                 <Card.Title style={{ textAlign: "center" }}>
-                    Walter white
+                    {props.user.name}
                 </Card.Title>
                 <Card.Text style={{ textAlign: "left" }}>
                     <strong>
                         Username:
-                    </strong> @AliNowrouzi
+                    </strong> @{props.user.username}
                 </Card.Text>
                 <Card.Text style={{ textAlign: "left" }}>
                     <strong>
                         Joined at:
-                    </strong> 2 day ago
+                    </strong> {format(props.user.createdAt)}
                 </Card.Text>
             </Card.Body>
             <Card.Body>
