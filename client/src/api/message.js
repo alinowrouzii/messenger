6d1fd4a071190966d628fc281e952f7a0c1eaa16
AliNowrouzi <alinowrouzii@gmail.com>
2021-07-25 01:32:45 +0430

    Modify getMessages route

--- client/src/api/message.js
+++ client/src/api/message.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 import { URL } from '../constants';
 
-export const getMessages = (chatId) => axios.get(`${URL}/message/${chatId}`, { withCredentials: true });
+export const getMessages = (chatId) => axios.get(`${URL}/message/getMessages/${chatId}`, { withCredentials: true });
 export const sendMessage = (newMessage) => axios.post(`${URL}/message/sendMessage`, newMessage, { withCredentials: true });
