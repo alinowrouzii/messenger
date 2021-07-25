56e90a271c7363f6a291be30734efd9f571f2e6d
AliNowrouzi <alinowrouzii@gmail.com>
2021-07-26 02:41:46 +0430

    Add getUsers to user route

--- server/route/user.js
+++ server/route/user.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { userIsLoggedIn } from '../controllers/authController.js';
-import { getUserData, getMe } from '../controllers/userController.js';
+import { getUserData, getUsersData, getMe } from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.get('/getUser/:userId', userIsLoggedIn, getUserData);
+router.get('/getUsers', userIsLoggedIn, getUsersData);
 router.get('/', userIsLoggedIn, getMe);
 
 export default router;
