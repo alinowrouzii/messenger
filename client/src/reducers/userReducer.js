f45d745c63be884aebc4b78624cf80edf6c35600
AliNowrouzi <alinowrouzii@gmail.com>
2021-07-26 02:36:52 +0430

    Add searchedUsers field to state

--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -2,15 +2,18 @@ import {
     FETCH_ME_SUCCESS,
     FETCH_ME_FAIL,
     SET_OWN_USER_READY,
+    SEARCH_USER_SUCCESS,
+    SEARCH_USER_FAIL,
     LOGOUT
 } from "../actions/types";
 
-const initialState = { ownUser: null, ownUserMessage:"", ownUserIsReady:false };
+const initialState = { ownUser: null, ownUserMessage: "", ownUserIsReady: false, searchedUsers: [] };
 
 export default (state = initialState, action) => {
+
     const { type, payload } = action;
     console.log(type, payload);
-    
+
     switch (type) {
         case FETCH_ME_SUCCESS:
             return {
@@ -28,6 +31,17 @@ export default (state = initialState, action) => {
                 ...state,
                 ownUserIsReady: true
             };
+        case SEARCH_USER_SUCCESS:
+            return {
+                ...state,
+                searchedUsers: payload.searchedUsers,
+                ownUserMessage: payload.message
+            }
+        case SEARCH_USER_FAIL:
+            return {
+                ...state,
+                ownUserMessage: payload.message
+            }
         // case FETCH_CONTACT_USERS_SUCCESS:
         //     return {
         //         ...state,
