4eff611d391af62006060eaf268f5115fe40e224
AliNowrouzi <alinowrouzii@gmail.com>
2021-07-26 02:35:03 +0430

    Add getUsers route

--- client/src/api/user.js
+++ client/src/api/user.js
@@ -3,4 +3,7 @@ import { URL } from '../constants';
 
 export const fetchMe = () => axios.get(`${URL}/user`, { withCredentials: true });
 export const fetchUserData = (userId) => axios.get(`${URL}/user/getUser/${userId}`, { withCredentials: true });
-export const fetchContactUsers = () => axios.get(`${URL}/user/getContactUsers`, { withCredentials: true });
+// export const fetchContactUsers = () => axios.get(`${URL}/user/getContactUsers`, { withCredentials: true }); // this route not exist in server route
+
+
+export const fetchUsers = (fieldName, regex) => axios.get(`${URL}/user/getUsers?${fieldName}=${regex}`, { withCredentials: true });
