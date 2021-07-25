d691e6fe7dcc23d0029c5d81794eeb4cf0f00add
AliNowrouzi <alinowrouzii@gmail.com>
2021-07-26 02:40:24 +0430

    Add getUsersData func and add some try catch block

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -8,7 +8,7 @@ export const getMe = async (req, res) => {
     try {
         const user = await User.findById(userId);
         if (user) {
-            return res.status(200).json({user,});
+            return res.status(200).json({ user, });
         }
         return res.status(404).json({ message: 'user not found!' });
     } catch (err) {
@@ -17,14 +17,56 @@ export const getMe = async (req, res) => {
 }
 
 export const getUserData = async (req, res) => {
-    const userId = mongoose.Types.ObjectId(req.params.userId);
 
     try {
-        const user = await User.findById(userId, { chats: 0, friends: 0 });
-        if (user) {
-            return res.status(200).json({user,});
+        const userId = mongoose.Types.ObjectId(req.params.userId);
+
+        try {
+
+            const user = await User.findById(userId);
+            if (user) {
+                return res.status(200).json({ user, });
+            }
+            return res.status(404).json({ message: 'user not found!' });
+        } catch (err) {
+            console.log(err);
+            return res.status(502).json({ message: 'DataBase error.' });
         }
-        return res.status(404).json({ message: 'user not found!' });
+
+    } catch (err) {
+        res.status(400).json({
+            message: "Argument passed in must be a single String of 12 bytes or a string of 24 hex characters"
+        });
+    }
+
+}
+
+export const getUsersData = async (req, res) => {
+
+    if (!req.query.username && !req.query.name) {
+        return res.status(400).json({ message: 'query not found!!' });
+    }
+
+    try {
+        if (req.query.username) {
+
+            const queryValue = req.query.username;
+
+            const users = await User.find({
+                username: { "$regex": queryValue, "$options": "i" }
+            });
+
+            return res.status(200).json({ users, });
+
+        } else {
+            const queryValue = req.query.name;
+
+            const users = await User.find({
+                name: { "$regex": queryValue, "$options": "i" }
+            });
+
+            return res.status(200).json({ users, });
+        }
     } catch (err) {
         console.log(err);
         return res.status(502).json({ message: 'DataBase error.' });
