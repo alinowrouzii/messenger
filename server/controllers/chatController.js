3903b85313e9e8a8083eff0adf4b48aac3177f5d
AliNowrouzi <alinowrouzii@gmail.com>
2021-07-26 02:41:04 +0430

    Add some try-catch block

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -26,37 +26,44 @@ export const getChats = async (req, res) => {
 
 export const createChat = async (req, res) => {
     const { userId } = req.body;
-    const userOneId = mongoose.Types.ObjectId(userId)
-    const userTwoId = req.user._id;
 
     try {
-        const userOne = await User.findById(userOneId).select('+friends').select('+chats');
-        const userTwo = await User.findById(userTwoId).select('+friends').select('+chats');
-        if (userOne && userTwo) {
-            if (!userOne.friends.some(element => userTwoId.equals(element))) {
-                const newChat = new Chat({ users: [userOneId, userTwoId] });
-                await newChat.save();
+        const userOneId = mongoose.Types.ObjectId(userId)
+        const userTwoId = req.user._id;
 
-                userOne.chats.push(newChat._id);
-                userOne.friends.push(userTwoId);
-                await userOne.save();
+        try {
+            const userOne = await User.findById(userOneId).select('+friends').select('+chats');
+            const userTwo = await User.findById(userTwoId).select('+friends').select('+chats');
+            if (userOne && userTwo) {
+                if (!userOne.friends.some(element => userTwoId.equals(element))) {
+                    const newChat = new Chat({ users: [userOneId, userTwoId] });
+                    await newChat.save();
 
-                userTwo.chats.push(newChat._id);
-                userTwo.friends.push(userOneId);
-                await userTwo.save();
+                    userOne.chats.push(newChat._id);
+                    userOne.friends.push(userTwoId);
+                    await userOne.save();
+
+                    userTwo.chats.push(newChat._id);
+                    userTwo.friends.push(userOneId);
+                    await userTwo.save();
 
 
-                const chat = await Chat.findById(newChat._id).populate('users').exec();
+                    const chat = await Chat.findById(newChat._id).populate('users').exec();
 
-                return res.status(201).json({ chat, message: 'New chat was created' });
+                    return res.status(201).json({ chat, message: 'New chat was created' });
+                } else {
+                    return res.status(400).json({ message: 'User already has a chat with given user!' });
+                }
             } else {
-                return res.status(400).json({ message: 'User already has a chat with given user!' });
+                return res.status(404).json({ message: 'User not found!' });
             }
-        } else {
-            return res.status(404).json({ message: 'User not found!' });
+        } catch (err) {
+            console.log(err);
+            return res.status(502).json({ message: 'Database error!' });
         }
-    } catch (err) {
-        console.log(err);
-        return res.status(502).json({ message: 'Database error!' });
+    }catch(err){
+        res.status(400).json({
+            message: "Argument passed in must be a single String of 12 bytes or a string of 24 hex characters"
+        });
     }
 };
--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -5,62 +5,72 @@ import User from "../models/User.js";
 
 export const sendMessage = async (req, res) => {
 
-    console.log('22')
-
     const { text, sender, chat } = req.body;
 
-    const senderId = mongoose.Types.ObjectId(sender);
-    const chatId = mongoose.Types.ObjectId(chat);
-    //check that loggedIn user_Id and sender User_Id mathched!
+    try {
+        const senderId = mongoose.Types.ObjectId(sender);
+        const chatId = mongoose.Types.ObjectId(chat);
+        //check that loggedIn user_Id and sender User_Id mathched!
 
-    if (senderId.equals(req.user._id)) {
-        try {
-            const fetchedChat = await Chat.findById(chatId);
-            if (fetchedChat) {//there's exist chat with that chat_id
+        if (senderId.equals(req.user._id)) {
+            try {
+                const fetchedChat = await Chat.findById(chatId);
+                if (fetchedChat) {//there's exist chat with that chat_id
 
-                if (fetchedChat.users.some(element => senderId.equals(element))) {//sender is member of that chat or not
+                    if (fetchedChat.users.some(element => senderId.equals(element))) {//sender is member of that chat or not
 
-                    const msg = new Message({ text, sender: senderId, chat: chatId });
-                    await msg.save();
-                    return res.status(201).json({
-                        messageInfo: 'Message sent!',
-                        message: msg
-                    });
+                        const msg = new Message({ text, sender: senderId, chat: chatId });
+                        await msg.save();
+                        return res.status(201).json({
+                            messageInfo: 'Message sent!',
+                            message: msg
+                        });
+                    } else {
+                        return res.status(404).json({ messageInfo: 'Can\'t send message to other chat!' });
+                    }
                 } else {
-                    return res.status(404).json({ messageInfo: 'Can\'t send message to other chat!' });
+                    return res.status(404).json({ messageInfo: 'Chat with that Id not found!' });
                 }
-            } else {
-                return res.status(404).json({ messageInfo: 'Chat with that Id not found!' });
-            }
 
-        } catch (err) {
-            console.log(err);
-            return res.status(502).json({ messageInfo: 'DataBase error on creating message' });
+            } catch (err) {
+                console.log(err);
+                return res.status(502).json({ messageInfo: 'DataBase error on creating message' });
+            }
+        } else {
+            return res.status(404).json({ messageInfo: 'Can\'t send message with diffrenet id!' });
         }
-    } else {
-        return res.status(404).json({ messageInfo: 'Can\'t send message with diffrenet id!' });
+    } catch (err) {
+        res.status(400).json({
+            message: "Argument passed in must be a single String of 12 bytes or a string of 24 hex characters"
+        });
     }
 
 }
 
 export const getMessages = async (req, res) => {
-    
-    const chatId = mongoose.Types.ObjectId(req.params.chatId);
-    const userId = req.user._id;
-    
-    try {
-        const currentUser = await User.findById(userId).select('+chats');
-        
-        //check that this chat is in user's chat list or not
-        if (currentUser.chats.some(element => chatId.equals(element))) {
-            const messages = await Message.find({ chat: chatId });
-            
-            return res.status(200).json({ messages, });
-        } else {
-            return res.status(404).json({ messageInfo: 'This chat not found in your chat list!!' })
-        }
-    } catch (err) {
-        return res.status(502).json({ messageInfo: 'DataBase error!' });
-    }
 
+    try {
+        const chatId = mongoose.Types.ObjectId(req.params.chatId);
+        const userId = req.user._id;
+
+        try {
+            const currentUser = await User.findById(userId).select('+chats');
+
+            //check that this chat is in user's chat list or not
+            if (currentUser.chats.some(element => chatId.equals(element))) {
+                const messages = await Message.find({ chat: chatId });
+
+                return res.status(200).json({ messages, });
+            } else {
+                return res.status(404).json({ messageInfo: 'This chat not found in your chat list!!' })
+            }
+        } catch (err) {
+            return res.status(502).json({ messageInfo: 'DataBase error!' });
+        }
+
+    } catch (err) {
+        res.status(400).json({
+            message: "Argument passed in must be a single String of 12 bytes or a string of 24 hex characters"
+        });
+    }
 }
