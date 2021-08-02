7c51e1553a7df8981f46869479f84a32c9064970
AliNowrouzi <alinowrouzii@gmail.com>
2021-08-03 00:22:15 +0430

    Add isTyping and stopTyping event to server side socket

--- server/socket/index.js
+++ server/socket/index.js
@@ -15,7 +15,10 @@ const socketConfig = (io, sessionMiddleware, passport) => {
             next();
         } else {
             console.log('failed in connecting socket')
-            next(new Error('unauthorized user!! --In-socket--'))
+            console.log(socket.id)
+            console.log('------------------------')
+            next(new Error('connect err'));
+            // next(new Error('unauthorized user!! --In-socket--'))
 
         }
     });
@@ -23,7 +26,7 @@ const socketConfig = (io, sessionMiddleware, passport) => {
 
     io.on('connect', (socket) => {
         // console.log(`new connection ${socket.id}`);
-        
+
         socket.on('whoami', (cb) => {
             cb(socket.request.user ? socket.request.user.username : '');
         });
@@ -32,39 +35,78 @@ const socketConfig = (io, sessionMiddleware, passport) => {
         socket.on("sendMessage", ({ sender, receiver, text }) => {
             console.log('message is sending...');
             console.log('users: ', users);
+            console.log(socket.id)
             console.log(new Date());
             console.log('---------------')
+
+            const senderr = socket.request.user;
             const user = getUser(receiver);
             if (user) {
                 io.to(user.socketId).emit("getMessage", {
-                    sender,
+                    sender: senderr._id.toString(),
                     text,
                 });
             }
         });
 
-
+        //TODO: get userId from session and then add user with that id
         socket.on("addUser", (userId, name) => {
             //try to add user
             console.log('--------try to add user---------')
+            console.log(new Date());
             console.log(users);
+            console.log('type of userId', typeof userId);
             addUser(userId, socket.id, name);
             console.log(users);
             console.log('--------------------------------')
             io.emit("getUsers", users);
         });
 
+
+        socket.on("isTyping", (receiver) => {
+            const user = getUser(receiver);
+            const ownUser = socket.request.user;
+            // console.log('ownuser', ownUser);
+            if (user) {
+                console.log('send typing')
+                io.to(user.socketId).emit("isTyping", {
+                    typingUser: ownUser._id.toString()
+                });
+            }
+        });
+
+        socket.on("stopTyping", (receiver) => {
+            const user = getUser(receiver);
+            const ownUser = socket.request.user;
+            // console.log(typeof ownUser)
+            if (user) {
+                console.log('send stop typing')
+                io.to(user.socketId).emit("stopTyping", {
+                    typingUser: ownUser._id.toString()
+                });
+            }
+        });
+
+
+
         // socket.on("disconnect", () => console.log("closed connection\n-------------------------------"))
 
         // when disconnect
         socket.on("disconnect", () => {
             console.log("user disconnected!");
-            console.log('---------------try to deleting-------------')
+            console.log('---------------try to deleting-------------');
             console.log(users);
             removeUser(socket.id);
             console.log(socket.id);
             console.log(users);
-            console.log('-------------------------------------------')
+
+            console.log('-------------------------------------------');
+
+            //TODO: find all friends of this user. and then emit getUsers to user online friends!!
+            //and then filter users array by friends of each users
+
+            //1-first specify that which users should recive getUsers event
+            //2-then specify that which users should be sent to that specified user
 
             io.emit("getUsers", users);
         });
