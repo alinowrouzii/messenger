a74115559787b01fc731e193c92f07606be5d7b8
AliNowrouzi <alinowrouzii@gmail.com>
2021-08-05 02:50:32 +0430

    Add audio and text message

--- server/models/Message.js
+++ server/models/Message.js
@@ -1,10 +1,16 @@
 import mongoose from 'mongoose';
+import { AUDIO_MESSAGE_TYPE, TEXT_MESSAGE_TYPE } from './constants.js';
+
+const options = {
+    timestamps: true,
+    discriminatorKey: 'kind'
+};
 
 const messageSchema = new mongoose.Schema({
-    text: {
-        type: String,
-        required: true
-    },
+    // text: {
+    //     type: String,
+    //     required: true
+    // },
     sender: {
         type: mongoose.Types.ObjectId,//holds user_Id
         ref: 'User'
@@ -14,8 +20,26 @@ const messageSchema = new mongoose.Schema({
         ref: 'Chat'
     }
 
-}, { timestamps: true });
+}, options);
 
-var Message = mongoose.model('Message', messageSchema);
+
+const Message = mongoose.model('Message', messageSchema);
+
+
+export const AudioMessage = Message.discriminator(AUDIO_MESSAGE_TYPE,
+    new mongoose.Schema({
+        data: {
+            type: Buffer,
+            required: true
+        },
+    }, options));
+
+export const TextMessage = Message.discriminator(TEXT_MESSAGE_TYPE,
+    new mongoose.Schema({
+        data: {
+            type: String,
+            required: true
+        },
+    }, options));
 
 export default Message;
---
+++ server/models/constants.js
@@ -0,0 +1,2 @@
+export const TEXT_MESSAGE_TYPE = 'TEXT_MESSAGE'
+export const AUDIO_MESSAGE_TYPE = 'AUDIO_MESSAGE'
