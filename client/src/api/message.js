abbd947189bda354e171828645bce03abfb8d508
AliNowrouzi <alinowrouzii@gmail.com>
2021-08-20 06:22:16 +0430

    Seperate send text message and voice message requests

--- client/src/api/message.js
+++ client/src/api/message.js
@@ -2,4 +2,16 @@ import axios from 'axios';
 import { URL } from '../constants';
 
 export const getMessages = (chatId) => axios.get(`${URL}/message/getMessages/${chatId}`, { withCredentials: true });
-export const sendMessage = (newMessage) => axios.post(`${URL}/message/sendMessage`, newMessage, { withCredentials: true });
+
+export const sendAudioMessage = ({ msg_type, chat, sender, form }) =>
+    axios.post(`${URL}/message/sendMessage`, form, {
+        withCredentials: true,
+        params: { msg_type, chat, sender },
+        headers: { "Content-Type": "multipart/form-data" }
+    });
+    
+export const sendTextMessage = ({ msg_type, chat, sender, data }) =>
+    axios.post(`${URL}/message/sendMessage`, { data, }, {
+        withCredentials: true,
+        params: { msg_type, chat, sender },
+    });
