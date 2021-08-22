ab4ea3ae08fa929da6e502e7328e137b0f4e84f1
AliNowrouzi <alinowrouzii@gmail.com>
2021-08-22 19:43:59 +0430

    Add image component to message

--- client/src/components/Utils/Message/Message.js
+++ client/src/components/Utils/Message/Message.js
@@ -1,17 +1,45 @@
 import React from 'react';
-import { Card, Spinner } from 'react-bootstrap';
+import { Card, Image, Spinner, Modal, Button } from 'react-bootstrap';
 import img from './../../../Images/no-profile.jpg';
 
 import { format } from "timeago.js";
 import './styles.css';
 import { useEffect, useState } from 'react';
 import { URL } from '../../../constants';
+import fs from 'fs';
+import request from 'request';
+import dateFormat from "dateformat";
+
 const Message = ({ me, msg, nextIsMe, nextIsUser }) => {
 
     const text_message_type = 'TEXT_MESSAGE'
     const audio_message_type = 'AUDIO_MESSAGE'
 
     const msg_type = msg.kind
+
+    useEffect(() => {
+        if (msg.kind === 'IMAGE_MESSAGE') {
+            console.log(`${URL}/message/getMedia/${msg.chat}/${msg._id}`)
+        }
+    });
+
+    const [imageModalShow, setImageModalShow] = useState(false);
+
+
+    // const handleDownloadImage = () => {
+    //     const url =  `${URL}/message/getMedia/${msg.chat}/${msg._id}`
+    //     const fileName = 'imagee'
+    //     request.head(url, function (err, res, body) {
+    //         console.log('content-type:', res.headers['content-type']);
+    //         console.log('content-length:', res.headers['content-length']);
+
+    //         request(url).pipe(fs.createWriteStream(fileName)).on('close', ()=>{
+    //             console.log('downloaded!')
+    //         });
+    //     });
+    // }
+
+    const [imageIsLoaded, setImageIsLoaded] = useState(false);
 
     return (
         <>
@@ -35,9 +63,9 @@ const Message = ({ me, msg, nextIsMe, nextIsUser }) => {
                                         className='float-end user-select-none text-white-50'
                                     >
                                         {msg.pending ?
-                                            <Spinner animation="border" variant="warning" style={{ width: '1rem', height: '1rem' }} />
+                                            <Spinner as='span' animation="border" variant="warning" style={{ width: '1rem', height: '1rem' }} />
                                             :
-                                            format(msg.createdAt)
+                                            dateFormat(new Date(msg.createdAt || ''), "h:MM TT")
                                         }
                                     </Card.Text>
 
@@ -48,15 +76,70 @@ const Message = ({ me, msg, nextIsMe, nextIsUser }) => {
                                 className="shadow-lg me-3 text-white my-card-cont"
                                 style={{ maxWidth: '18rem', float: 'right', backgroundColor: "#3581eb" }}>
                                 <Card.Body
-                                    className='ps-2 pt-1'
+                                    className='p-0 d-flex justify-content-center mb-0'
                                 >
-                                    <audio className='my-audio mb-0' src={msg.url || `${URL}/message/getAudio/${msg.chat}/${msg._id}`} controls controlsList="nodownload" />
+                                    {msg_type === audio_message_type ?
+                                        <audio className='my-audio mb-0 pb-0' src={msg.url || `${URL}/message/getMedia/${msg.chat}/${msg._id}`} controls controlsList="nodownload" />
+                                        :
+                                        <>
+                                            {/* {!imageIsLoaded &&
+                                                <div
+                                                    style={{
+                                                        background: 'blue',
+                                                        minWidth: '15rem',
+                                                        minHeight: '15rem',
+                                                        maxWidth: '100%', height: 'auto',
+                                                        cursor: 'pointer',
+                                                        borderTopLeftRadius: '10px',
+                                                        borderTopRightRadius: '10px'
+                                                    }}
+                                                />
+                                            } */}
+                                            <Image
+                                                className='mb-0 pb-0 '
+                                                // className={'mb-0 pb-0 ' + (!imageIsLoaded && ' d-none')}
+                                                // className='rounded'
+                                                src={msg.url || `${URL}/message/getMedia/${msg.chat}/${msg._id}`}
+                                                style={{
+                                                    minWidth: '15rem',
+                                                    minHeight: '15rem',
+                                                    maxWidth: '100%', height: 'auto',
+                                                    cursor: 'pointer',
+                                                    borderTopLeftRadius: '10px',
+                                                    borderTopRightRadius: '10px'
+                                                }}
+                                                onClick={() => setImageModalShow(true)}
+                                                onLoad={() => setImageIsLoaded(true)}
+                                            />
 
+                                            <Modal
+                                                show={imageModalShow}
+                                                aria-labelledby="contained-modal-title-vcenter"
+                                                centered
+                                                size='lg'
+                                            >
+                                                <Modal.Body className='d-flex justify-content-center'>
+                                                    <Image
+                                                        className='rounded border border-secondary'
+                                                        src={msg.url || `${URL}/message/getMedia/${msg.chat}/${msg._id}`}
+                                                        style={{ maxWidth: '100%', height: 'auto' }}
+                                                        onClick={() => setImageModalShow(true)}
+                                                    />
+                                                </Modal.Body>
+
+                                                <Modal.Footer>
+                                                    <Button onClick={() => setImageModalShow(false)}>Close</Button>
+                                                </Modal.Footer>
+
+                                            </Modal>
+                                        </>
+
+                                    }
                                 </Card.Body>
-                                
+
                                 {msg.text.trim().length > 0 &&
                                     <Card.Body
-                                        className="pt-1 ps-2"
+                                        className="pt-1 ps-2 pb-0"
                                     >
                                         {msg.text}
                                     </Card.Body>
@@ -69,9 +152,9 @@ const Message = ({ me, msg, nextIsMe, nextIsUser }) => {
                                     >
 
                                         {msg.pending ?
-                                            <Spinner animation="border" variant="warning" style={{ width: '1rem', height: '1rem' }} />
+                                            <Spinner as='span' animation="border" variant="warning" style={{ width: '1rem', height: '1rem' }} />
                                             :
-                                            format(msg.createdAt)
+                                            dateFormat(new Date(msg.createdAt || ''), "h:MM TT")
                                         }
                                     </Card.Text>
 
@@ -106,7 +189,7 @@ const Message = ({ me, msg, nextIsMe, nextIsUser }) => {
                                     <Card.Text
                                         className='float-end text-muted user-select-none'
                                     >
-                                        {format(msg.createdAt)}
+                                        {dateFormat(new Date(msg.createdAt || ''), "h:MM TT")}
                                     </Card.Text>
 
                                 </Card.Body>
@@ -116,15 +199,56 @@ const Message = ({ me, msg, nextIsMe, nextIsUser }) => {
                                 className="shadow-lg user-card-cont"
                                 style={{ float: "left", maxWidth: '18rem', backgroundColor: "#c5c5c3" }}>
                                 <Card.Body
-                                    className="pt-1 ps-2"
+                                    className="p-0 d-flex justify-content-center mb-0"
                                 >
-                                    <audio className='user-audio' src={`${URL}/message/getAudio/${msg.chat}/${msg._id}`} controls controlsList="nodownload" />
+
+                                    {msg_type === audio_message_type ?
+                                        <audio className='user-audio mb-0 pb-0' src={`${URL}/message/getMedia/${msg.chat}/${msg._id}`} controls controlsList="nodownload" />
+                                        :
+                                        <>
+                                            <Image
+                                                className='pb-0 mb-0'
+                                                src={msg.url || `${URL}/message/getMedia/${msg.chat}/${msg._id}`}
+                                                style={{
+                                                    minWidth: '15rem',
+                                                    minHeight: '15rem',
+                                                    maxWidth: '100%',
+                                                    height: 'auto',
+                                                    cursor: 'pointer',
+                                                    borderTopLeftRadius: '10px',
+                                                    borderTopRightRadius: '10px'
+                                                }}
+                                                onClick={() => setImageModalShow(true)}
+                                            />
+                                            <Modal
+                                                show={imageModalShow}
+                                                aria-labelledby="contained-modal-title-vcenter"
+                                                centered
+                                                size='lg'
+                                            >
+                                                <Modal.Body className='d-flex justify-content-center'>
+                                                    <Image
+                                                        className='rounded border border-secondary'
+                                                        src={msg.url || `${URL}/message/getMedia/${msg.chat}/${msg._id}`}
+                                                        style={{ maxWidth: '100%', height: 'auto' }}
+                                                        onClick={() => setImageModalShow(true)}
+                                                    />
+                                                </Modal.Body>
+
+                                                <Modal.Footer>
+                                                    <Button onClick={() => setImageModalShow(false)}>Close</Button>
+                                                </Modal.Footer>
+
+                                            </Modal>
+                                        </>
+
+                                    }
 
                                 </Card.Body>
 
                                 {msg.text.trim().length > 0 &&
                                     <Card.Body
-                                        className="pt-1 ps-2"
+                                        className="pt-1 ps-2 pb-0"
                                     >
                                         {msg.text}
                                     </Card.Body>
@@ -133,7 +257,7 @@ const Message = ({ me, msg, nextIsMe, nextIsUser }) => {
                                     <Card.Text
                                         className='float-end text-muted user-select-none'
                                     >
-                                        {format(msg.createdAt)}
+                                        {dateFormat(new Date(msg.createdAt || ''), "h:MM TT")}
                                     </Card.Text>
 
                                 </Card.Body>
