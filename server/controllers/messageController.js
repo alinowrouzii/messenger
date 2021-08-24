b52dbf321c3bba9b0b7caa9209d3c7d67f287ecf
AliNowrouzi <alinowrouzii@gmail.com>
2021-08-24 22:05:33 +0430

    Add getMedia handler for getMedia route

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 import multerConfig from '../config/multer.js';
 import multer from 'multer';
 import Chat from "../models/Chat.js";
-import { AUDIO_MESSAGE_TYPE, TEXT_MESSAGE_TYPE } from '../models/constants.js';
-import Message, { AudioMessage, TextMessage } from "../models/Message.js";
+import { AUDIO_MESSAGE_TYPE, TEXT_MESSAGE_TYPE, IMAGE_MESSAGE_TYPE } from '../models/constants.js';
+import Message from "../models/Message.js";
 import User from "../models/User.js";
 import mime from 'mime-types';
 import fse from 'fs-extra';
@@ -33,16 +33,17 @@ export const sendMessage = async (req, res) => {
 
                     if (fetchedChat.users.some(element => senderId.equals(element))) {//sender is member of that chat or not
 
-                        if (msg_type === AUDIO_MESSAGE_TYPE) {
+                        if (msg_type === AUDIO_MESSAGE_TYPE || msg_type === IMAGE_MESSAGE_TYPE) {
 
                             let dest = '/uploads/chats';
-                            dest = path.join(__dirname, '..', dest, chat);
-                            console.log(dest)
+                            dest = path.join(__dirname, '..', dest, chat, msg_type);
+                            console.log(dest);
                             // audio max size is 3MB
                             const maxSize = 3 * 1024 * 1024;
-                            const upload = multerConfig(dest, maxSize, ['ogg', 'png']);
+                            const acceptableTypes = (msg_type === AUDIO_MESSAGE_TYPE) ? ['ogg', 'webm', 'weba'] : ['jpg', 'jpeg', 'png', 'webp'];
+                            const upload = multerConfig(dest, maxSize, acceptableTypes);
 
-                            upload.single('voice')(req, res, async function (err) {
+                            upload.single(msg_type)(req, res, async function (err) {
                                 if (err) {
                                     console.log(err.message || err.msg);
                                     if (err instanceof multer.MulterError) {
@@ -52,7 +53,7 @@ export const sendMessage = async (req, res) => {
                                     }
 
                                     if (err.msg === 'File too large') {
-                                        return res.status(413).json({ messageInfo: `File is larger than ${maximumFileSize / (1024 * 1024)}MB` });
+                                        return res.status(413).json({ messageInfo: `File is larger than ${maxSize / (1024 * 1024)}MB` });
                                     }
                                     return res.status(406).json({ messageInfo: `upload failed! due to an error: ${err.message || err.msg}` });
                                 }
@@ -65,25 +66,37 @@ export const sendMessage = async (req, res) => {
 
                                 const filename = req.file.filename;
 
-                                const msg = new AudioMessage({ audioName: filename, sender: senderId, chat: chatId });
+                                const { text } = req.body;
+                                const msg = new Message({
+                                    text,
+                                    messageDataName: filename,
+                                    sender: senderId,
+                                    chat: chatId,
+                                    kind: msg_type
+                                });
                                 await msg.save();
 
                                 return res.status(201).json({
                                     messageInfo: 'Message created!',
-                                    message: msg
+                                    messageId: msg._id
                                 });
 
                             });
                         } else if (msg_type === TEXT_MESSAGE_TYPE) {
 
-                            const { data } = req.body;
+                            const { text } = req.body;
 
-                            const msg = new TextMessage({ data, sender: senderId, chat: chatId });
+                            const msg = new Message({
+                                text,
+                                sender: senderId,
+                                chat: chatId,
+                                kind: TEXT_MESSAGE_TYPE
+                            });
 
                             await msg.save();
                             return res.status(201).json({
                                 messageInfo: 'Message created!',
-                                message: msg
+                                messageId: msg._id
                             });
                         }
 
@@ -142,8 +155,9 @@ export const uploadTest = (req, res) => {
 
 }
 
-export const getAudio = async (req, res) => {
+export const getMedia = async (req, res) => {
 
+    // setTimeout(async () => {
     try {
         const chatId = mongoose.Types.ObjectId(req.params.chatId);
         const messageId = mongoose.Types.ObjectId(req.params.msgId);
@@ -156,23 +170,37 @@ export const getAudio = async (req, res) => {
             if (currentUser.chats.some(element => chatId.equals(element))) {
                 const message = await Message.findOne({ chat: chatId, _id: messageId });
 
-                if (!message || message.kind !== AUDIO_MESSAGE_TYPE) {
+                if (!message || (message.kind !== AUDIO_MESSAGE_TYPE && message.kind !== IMAGE_MESSAGE_TYPE)) {
                     return res.status(404).json({ messageInfo: 'message no found!' });
                 }
 
-                const file = message.audioName;
+                const file = message.messageDataName;
                 const filename = path.basename(file);
                 const mimetype = mime.lookup(file);
 
                 let filePath = '/uploads/chats';
                 const chat = req.params.chatId;
-                filePath = path.join(__dirname, '..', filePath, chat, file);
+                filePath = path.join(__dirname, '..', filePath, chat, message.kind, file);
 
-                res.setHeader('Content-disposition', 'attachment; filename=' + filename);
-                res.setHeader('Content-type', mimetype);
+                fse.stat(filePath, function (err) {
+                    if (err == null) {
+                        res.setHeader('Content-disposition', 'attachment; filename=' + filename);
+                        res.setHeader('Content-type', mimetype);
 
-                const filestream = fse.createReadStream(filePath);
-                filestream.pipe(res);
+                        const filestream = fse.createReadStream(filePath);
+                        filestream.pipe(res);
+                        return;
+                    } else if (err.code === 'ENOENT') {
+                        console.log('not found!')
+                        return res.status(404).json({ message: 'Media not found!' });
+                        // return resolve
+                        (false);
+                    } else {
+                        return reject(err);
+                    }
+                })
+
+
             } else {
                 return res.status(404).json({ messageInfo: 'This chat not found in your chat list!!' })
             }
@@ -186,4 +214,5 @@ export const getAudio = async (req, res) => {
             message: "Argument passed in must be a single String of 12 bytes or a string of 24 hex characters"
         });
     }
+    // }, 5000);
 }
