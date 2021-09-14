075c01ad181d2a0e869bf4e8eba1d5a57fd67c54
alinowrouzii <alinowrouzii@gmail.com>
2021-09-15 01:47:06 +0430

    Update

--- server/config/mongooseConfig.js
+++ server/config/mongooseConfig.js
@@ -1,10 +1,15 @@
 import mongoose from 'mongoose';
 
 export const mongooseConfig = async () => {
-    const MONGIOSE_URL = process.env.MONGOOSE_URL_2;
+    let MONGO_URL;
+    if (process.env.NODE_ENV === "development") {
+        MONGO_URL = `mongodb://localhost:27017/DevDB`
+    } else {
+        MONGO_URL = process.env.MONGO_URL;
+    }
 
     try {
-        await mongoose.connect(MONGIOSE_URL, {
+        await mongoose.connect(MONGO_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true,
