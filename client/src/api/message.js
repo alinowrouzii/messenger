9744e03de5e6b140c2dd02f2e722790a6fb504a3
alinowrouzii <alinowrouzii@gmail.com>
2021-09-15 01:11:58 +0430

    update message

--- client/src/api/message.js
+++ client/src/api/message.js
@@ -3,15 +3,15 @@ import { URL } from '../constants';
 
 export const getMessages = (chatId) => axios.get(`${URL}/message/getMessages/${chatId}`, { withCredentials: true });
 
-export const sendAudioMessage = ({ kind:msg_type, chat, sender, form }) =>
+export const sendMediaMessage = ({ kind: msg_type, chat, sender, form }) =>
     axios.post(`${URL}/message/sendMessage`, form, {
         withCredentials: true,
         params: { msg_type, chat, sender },
         headers: { "Content-Type": "multipart/form-data" }
     });
 
-export const sendTextMessage = ({ kind: msg_type, chat, sender, text }) =>
-    axios.post(`${URL}/message/sendMessage`, { text, }, {
+export const sendTextMessage = ({ kind: msg_type, chat, sender, text, repliedMessage }) =>
+    axios.post(`${URL}/message/sendMessage`, { text, repliedMessage }, {
         withCredentials: true,
         params: { msg_type, chat, sender },
     });
